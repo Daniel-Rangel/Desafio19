ea90467a2a4ac3984a8b0c67ed129b51624eaf8f
Daniel Rangel <danf.arangel@gmail.com>
2022-02-12 23:34:00 -0300

    DOM/eventos/manipulação

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-/* Condicionais */
+const button = document.querySelector('.btn-js')
+const namee = document.querySelector('.nome-js')
+const email = document.querySelector('.email-js')
 
-const sum = 1+2
+const itens = document.querySelectorAll('.item')
 
-if( sum === 2){
-    console.log("foi")
-}else if(sum === 3){
-    console.log("foi mais um")
-}else{
-    console.log("não foi")
-}
+// click recebe 2 parametros um o evento e outro uma função
+button.addEventListener('click', (e)=>{
+    e.preventDefault() // impede o envio pelo form quando usado input type="button"
+    
+    const nameV = namee.value
+    const emailV = email.value
+
+    if( nameV === "" || emailV === ""){
+        return console.log("Não tem valores")
+    }
+    
+    console.log(nameV)
+    itens.firstElementChild.textContent = nameV
+    itens.children[1].textContent = emailV
+})
+
