b61d91286c997202220454b642f310f33e81dc2d
Daniel Rangel <danf.arangel@gmail.com>
2022-02-15 22:07:47 -0300

    desafio do natão

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const nameImput = document.querySelector('.nome-js')
+/* const nameImput = document.querySelector('.nome-js')
 const emailImput = document.querySelector('.email-js')
 const btnImput = document.querySelector('.btn-js')
 // const myForm = document.querySelector('.myForm')
@@ -31,3 +31,68 @@ btnImput.addEventListener('click', (e)=>{
         emailImput.value  = ''
     }
 })
+ */
+
+/* CALCULO IMC */
+const form = document.querySelector('.IMC_calc form')
+const alturaInput = document.querySelector('.altura-js')
+const pesoInput = document.querySelector('.peso-js')
+const calcBtn = document.querySelector('button.calc_btn')
+const resposta = document.querySelector('.resposta-js')
+
+
+
+/* calculo */
+const calculoIMC = (altura , peso)=> {
+    const calculo =   peso / (altura * altura) 
+    
+    if(calculo < 16){
+       return 'Magreza grave' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 16 || calculo < 17){
+        return 'Magreza moderada' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 17 || calculo < 18.5){
+        return 'Magreza Leve' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 18.5 || calculo < 25){
+        return 'Saudável' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 25 || calculo < 30){
+        return 'Sobrepeso' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 30 || calculo < 35){
+        return 'Obesidade Grau I' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo <= 35 || calculo < 40){
+        return 'Obesidade Grau II' + ' ' + calculo.toFixed(2)
+    }
+
+    if(calculo >= 40){
+        return 'Obesidade Grau III' + ' ' + calculo.toFixed(2)
+    }
+    
+}
+
+
+calcBtn.addEventListener('click', (e)=> {
+    e.preventDefault()
+
+    const altura = alturaInput.value.replace(',','.')
+    const peso = pesoInput.value
+
+    if(altura === '' || peso === ''){
+        resposta.innerText = 'Preencha os campos acima'
+        resposta.style.color = 'red'
+    }
+
+    resposta.innerHTML = ` ${calculoIMC(altura,peso)}`
+})
+
+
